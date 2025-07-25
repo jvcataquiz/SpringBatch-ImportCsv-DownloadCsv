393357f3f0d9e991535971303121c8a28d1e4b98
jvcataquiz <jvcataquiz@gmail.com>
2025-07-25 09:28:28 +0800

    Add CSV import process using Spring Batch

---
+++ spring-batch-example-main/src/main/java/com/spring/batch/example/controller/JobController.java
@@ -0,0 +1,62 @@
+package com.spring.batch.example.controller;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+@RequestMapping("/jobs")
+public class JobController {
+
+    @Autowired
+    private JobLauncher jobLauncher;
+    @Autowired
+    @Qualifier("importCSVJob")
+    private Job importCSVJob;
+    @Value("${file.upload.dir}")
+    private String uploadDir;
+    private Path uploadPath;
+
+    public void init() throws Exception{
+        uploadPath = Paths.get(uploadDir).toAbsolutePath().normalize();
+        Files.createDirectories(uploadPath);
+    }
+    public File getFile(String fileName){
+        return uploadPath.resolve(Paths.get(fileName)).toFile().getAbsoluteFile();
+    }
+    @PostMapping("/importCustomers")
+    public void importCsvToDBJob(@RequestParam("file") MultipartFile file) throws IOException {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        File fileToImport = this.getFile(fileName);
+        file.transferTo(fileToImport);
+
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("fullPathFileName", fileToImport.getAbsolutePath())
+                .addLong("startAt", System.currentTimeMillis()).toJobParameters();
+        try {
+            jobLauncher.run(importCSVJob, jobParameters);
+        } catch (JobExecutionAlreadyRunningException | JobRestartException | JobInstanceAlreadyCompleteException | JobParametersInvalidException e) {
+            e.printStackTrace();
+        }
+    }
+}
